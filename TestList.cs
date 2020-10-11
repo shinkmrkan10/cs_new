9249a8be0638d6853c065772d4fff10ea8b1690c
shinkmrkan10 <shinkmrkan10@gmail.com>
2020-10-11 15:22:57 +0900

    data adding

--- TestList.cs
+++ TestList.cs
@@ -8,8 +8,14 @@ public class TestList {
         public TestList() {
             Data = new ObservableCollection<Test> {
                 new Test { Subj="国語", Points=90, Name="田中　一郎", ClassName="A" },
-                new Test { Subj="数学", Points=50, Name="鈴木　二郎", ClassName="A" },
-                new Test { Subj="英語", Points=90, Name="佐藤　三郎", ClassName="B" }
+                new Test { Subj="国語", Points=80, Name="鈴木　二郎", ClassName="A" },
+                new Test { Subj="国語", Points=70, Name="佐藤　三郎", ClassName="B" },
+                new Test { Subj="数学", Points=50, Name="田中　一郎", ClassName="A" },
+                new Test { Subj="数学", Points=60, Name="鈴木　二郎", ClassName="A" },
+                new Test { Subj="数学", Points=80, Name="佐藤　三郎", ClassName="B" },
+                new Test { Subj="英語", Points=70, Name="田中　一郎", ClassName="A" },
+                new Test { Subj="英語", Points=90, Name="鈴木　二郎", ClassName="A" },
+                new Test { Subj="英語", Points=60, Name="佐藤　三郎", ClassName="B" }
             };
         }       
  }
